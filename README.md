d5982e815edfd0738c6f317efa56c3aac26b48d0
thesurajdas <suraj4apps@gmail.com>
2024-01-11 02:15:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tailwind CSS and Next.js Portfolio
+
+This is my portfolio with Tailwind CSS and Next.js.
+
+## Running Locally
+
+Can run the application in VS Code or a terminal and it will be available at `http://localhost:3000`.
+
+```bash
+npm install
+npm run dev
+```
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,89 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import SlideUp from "./SlideUp"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+    name: "Carecure24",
+    description:
+      "It is a appoinment booking system for Homecare and Medical Services",
+    image: "/src1.png",
+    github: "https://github.com/thesurajdas/Carecure24",
+    link: "https://carecure24.com",
+  },
+  {
+    name: "Image Sharing and Management Website",
+    description: "This is a image sharing and management web application project.",
+    image: "/src2.jpg",
+    github: "https://github.com/thesurajdas/imagex",
+    link: "",
+  },
+  {
+    name: "Smart Text Editor For Sign Language Users",
+    description:
+      "It is a Text Editor for Sign Language Users using Computer Vision",
+    image: "/src3.png",
+    github: "https://github.com/thesurajdas/text-editor-sign-language",
+    link: "",
+  },
+]
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-violet-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <Link href={project.link}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link?project.link:'#'} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          )
+        })}
+        
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
